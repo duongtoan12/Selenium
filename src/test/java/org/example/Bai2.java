51d8f504e2268284165af4a0b7d9e8803df6dbf4
duongtoan12 <duongdinhtoan0310@gmail.com>
2022-04-20 23:01:01 +0700

    bai2

---
+++ src/test/java/org/example/Bai2.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Bai2 {
+
+    WebDriver Driver;
+    @Before
+    public void Before(){
+        WebDriverManager.chromedriver().setup();
+        this.Driver = new ChromeDriver();
+        this.Driver.manage().window().maximize();
+        this.Driver.get("https://www.oanda.com/currency-converter/en/?from=EUR&to=USD&amount=1");
+    }
+
+    @After
+    public void AfterTest() throws InterruptedException{
+        Thread.sleep(5000);
+        this.Driver.quit();
+    }
+
+    @Test
+    public void CheckvalidTime(){
+        WebElement tbbutton =this.Driver.findElement(By.cssSelector("#cc-main-conversion-block > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2 > div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-align-items-xs-center.MuiGrid-grid-xs-12.MuiGrid-grid-sm-6 > div.react-datepicker-wrapper.cc37 > div > div > div.MuiInputAdornment-root.MuiInputAdornment-positionStart > button > span.MuiIconButton-label > svg"))
+    }
+
+}
--- src/test/java/org/example/BtBuoi9.java
+++ src/test/java/org/example/BtBuoi9.java
@@ -12,8 +12,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BtBuoi9 {
     WebDriver Driver;
-    String URL_login = "http://localhost/wp/wp-login.php";
-
     @Before
     public void Test_befor(){
         WebDriverManager.chromedriver().setup();
@@ -37,16 +35,35 @@ public class BtBuoi9 {
         TbEmail.sendKeys("");
         WebElement TbPass = this.Driver.findElement(By.cssSelector("input[type=\"password\"]"));
         TbPass.sendKeys("");
-        this.Driver.findElement(By.cssSelector("button[type=\"submit\"]"));
-        WebElement LinkText = this.Driver.findElement(By.linkText("Vui lòng nhập dữ liệu"));
-        Assert.assertEquals(true, LinkText);
+        this.Driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+        String Null = this.Driver.findElement(By.id("auth-block__form-group__email-error")).getText();
+        Assert.assertEquals(Null,"Vui lòng nhập dữ liệu");
+//
+    }
 
 
 
+    @Test
+    public void CheckInvalidEmail(){
+        WebElement TbEmail = this.Driver.findElement(By.cssSelector("input#auth-block__form-group__email"));
+        TbEmail.sendKeys("duongdinhtoan@gmail.com");
+        WebElement TbPass = this.Driver.findElement(By.cssSelector("input[type=\"password\"]"));
+        TbPass.sendKeys("Abc123");
+        this.Driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+        String InvalidEmail = this.Driver.findElement(By.xpath("//*[@id=\"auth-block__login-form\"]/div[1]/text()")).getText();
+        Assert.assertEquals(InvalidEmail,"- Tài khoản không tồn tại, vui lòng kiểm tra lại");
+    }
 
-//        String ExpectedURL = Driver.getCurrentUrl();
-//        Assert.assertTrue(ExpectedURL.startsWith("https://fado.vn/dang-nhap?redirect=https%3A%2F%2Ffado.vn%2F"));
 
+    @Test
+    public void CheckInvalidPass(){
+        WebElement TbEmail = this.Driver.findElement(By.cssSelector("input#auth-block__form-group__email"));
+        TbEmail.sendKeys("duongdinhtoan0310@gmail.com");
+        WebElement TbPass = this.Driver.findElement(By.cssSelector("input[type=\"password\"]"));
+        TbPass.sendKeys("Abc12");
+        this.Driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+        String TbInvalidPass = this.Driver.findElement(By.xpath("//*[@id=\"auth-block__login-form\"]/div[1]/text()")).getText();
+        Assert.assertEquals(TbInvalidPass,"- Mật khẩu không đúng, vui lòng kiểm tra lại");
     }
 
     @Test
@@ -58,12 +75,8 @@ public class BtBuoi9 {
         WebElement TbPass = this.Driver.findElement(By.cssSelector("input[type=\"password\"]"));
         TbPass.sendKeys("Abc123");
         this.Driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
-        this.Driver.findElement(By.cssSelector("span.user-name-col")).click();
-        this.Driver.findElement(By.cssSelector("a.user-name-label")).click();
-        String ExpectedURL = Driver.getCurrentUrl();
-
-        Assert.assertTrue(ExpectedURL.startsWith("https://fado.vn/quan-ly-tai-khoan"));
-
+        String LoginSuccess = this.Driver.findElement(By.xpath("//*[@id=\"user-info__dropdown\"]/div[1]/span[1]")).getText();
+        Assert.assertEquals(LoginSuccess,"Toàn Dương");
     }
 
 
